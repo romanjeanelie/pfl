8c6a9e64255c85cd6175484623fb3b3e9241cffd
roman7501 <64649303+roman7501@users.noreply.github.com>
2021-07-26 21:28:51 +0200

    better grid and responsive

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ gsap.registerPlugin(SplitText);
 const projectLinks = document.querySelectorAll(".projects__container ul li a");
 const contactLinks = document.querySelectorAll(".contact__container ul li a");
 
-console.log(SplitText);
-const lettersConstruction = new SplitText(".construction", { type: "chars" });
+// const lettersConstruction = new SplitText(".construction", { type: "chars" });
 const lettersName = new SplitText(".name h1", { type: "words" });
 const lettersNameInfo = new SplitText(".name p", { type: "words" });
 const lettersProjectTitle = new SplitText(".projects__container h2", { type: "words, chars" });
@@ -15,53 +14,44 @@ const lettersContactTitle = new SplitText(".contact__container h2", { type: "wor
 
 const tl = gsap.timeline();
 
-gsap.from(lettersConstruction.chars, {
-  rotateY: "90deg",
-  delay: 0.3,
-  stagger: 0.07,
-  duration: 7,
-  ease: "back.out",
+gsap.from(".construction", {
+  opacity: 0,
+  duration: 4,
+  delay: 0.7,
+  // ease: "power2.out",
 });
-
 gsap.from(lettersName.words, {
-  y: 40,
+  y: 60,
   duration: 2,
+  stagger: 0,
 });
+
 gsap.from(lettersNameInfo.words, {
-  y: 40,
-  delay: 0.5,
-  duration: 2.4,
-});
-gsap.from(lettersProjectTitle.chars, {
-  y: 20,
-  duration: 0.5,
-  stagger: 0.1,
+  y: 60,
+  duration: 2,
+  stagger: 0,
 });
 
-projectLinks.forEach((link) => {
-  const linkLetters = new SplitText(link, { type: "chars" });
-
-  gsap.from(linkLetters.chars, {
-    y: 20,
-    delay: 0.2,
-    stagger: 0.03,
-    duration: 0.5,
-  });
+gsap.from(lettersContactTitle.words, {
+  y: 60,
+  duration: 2,
+  stagger: 0.3,
 });
 
-contactLinks.forEach((link) => {
-  const linkLetters = new SplitText(link, { type: "chars" });
-  gsap.from(linkLetters.chars, {
-    y: 20,
-    delay: 0.2,
-    stagger: 0.02,
-    duration: 0.5,
-    ease: "linear",
-  });
+gsap.from(lettersProjectTitle.words, {
+  y: 60,
+  duration: 2,
+  stagger: 0.3,
 });
 
-gsap.from(lettersContactTitle.chars, {
-  y: 20,
-  duration: 0.5,
-  stagger: 0.1,
+gsap.from(".projects__container ul li a", {
+  y: 60,
+  duration: 2,
+  stagger: 0.3,
+});
+
+gsap.from(".contact__container ul li a", {
+  y: 60,
+  duration: 2,
+  stagger: 0.3,
 });
